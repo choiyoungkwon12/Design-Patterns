6da13c02e9d94dd87e3947c71ca1129af71bf709
choiyoungkwon12 <47075043+choiyoungkwon12@users.noreply.github.com>
2021-08-13 12:03:07 +0900

    Update Main.java

--- src/main/java/com/programmers/designpattern/begavioral/iterator/Main.java
+++ src/main/java/com/programmers/designpattern/begavioral/iterator/Main.java
@@ -5,7 +5,7 @@ public class Main {
         BookShelf<Book> bookShelf = new BookShelf(4);
         bookShelf.appendBook(new Book("Java의 정석"));
         bookShelf.appendBook(new Book("토비의 스프링"));
-        bookShelf.appendBook(new Book("웹 개발을 위한 git,github 시작하기"));
+        bookShelf.appendBook(new Book("팀 개발을 위한 git,github 시작하기"));
         bookShelf.appendBook(new Book("처음 배우는 스프링 부트2"));
 
         Iterator<Book> iterator = bookShelf.iterator();
