f8ab2cbc3cbbed0656ba7fe071d1417f090dbd1a
choiyoungkwon12 <hpwh4457@naver.com>
2021-09-02 03:45:17 +0900

    코드 정리

--- src/main/java/com/programmers/designpattern/begavioral/observer/EventSource.java
+++ src/main/java/com/programmers/designpattern/begavioral/observer/EventSource.java
@@ -1,11 +1,13 @@
 package com.programmers.designpattern.begavioral.observer;
 
-import java.util.Observable; // 이 부분이 옵저버에게 신호를 보내는 주체입니다.
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Observable;
 
 public class EventSource extends Observable implements Runnable {
+
+    @Override
     public void run() {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
@@ -18,4 +20,5 @@ public class EventSource extends Observable implements Runnable {
             e.printStackTrace();
         }
     }
+
 }
